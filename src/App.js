40f6a32d068d0e5e9db21ba8be52c3f3661acf1e
Krinjal Patel <krinjal.patel@bookmyshow.com>
2017-12-26 15:20:01 +0530

    searchBar : Used external CSS library i.e. google material ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import {GridList, GridTile} from 'material-ui/GridList';
+import AppBar from 'material-ui/AppBar';
+import Word from './words.json';
 import './App.css';
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           <input />
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    // width: 500,
+    // height: 450,
+    width: 400,
+    height: 250,
+    overflowY: 'auto',
+  },
+};
+
+class App extends Component {  
+  constructor(props){
+      super(props);
+      this.state ={
+        term : '',list : Word,
+        search : ''
+      };
+    }
+   
+  updatedData(event){
+    this.setState({search: event.target.value})
+  }
+render() {
+  let filterList = this.state.list.filter((item) => {
+        return item.word.indexOf(this.state.search) !== -1; 
+      }
+    );
+  var flower = filterList.map(function(item){
+    if(item.ratio === -1){
+      return null;
+    } 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <GridList
+          key={item.id}
+          cellHeight={180}
+          style={styles.gridList}
+        >      
+          <GridTile
+            title={item.word}
+            subtitle={item.meaning}
+          >
+          <img src={"http://appsculture.com/vocab/images/"+ item.id +".png"} alt=""/>
+          </GridTile>
+        </GridList>
+    );
+  });
+    return (
+      <MuiThemeProvider>
+        <div>
+          <AppBar
+            title="Search App"
+          />
+          <div className='searchBar'>
+            <TextField floatingLabelText="Search Dictionary" name="search" value = {this.state.search}
+              onChange={this.updatedData.bind(this)}
+            />
+          </div>
+          <div style={styles.root}>{flower}</div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
